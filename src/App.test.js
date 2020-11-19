9ae73cdbcae8c260ea9aeeea34c8aa9b92a98cf7
benlewisalex <36609047+benlewisalex@users.noreply.github.com>
2020-11-18 22:30:33 -0600

    please hire me 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi Parker I just deployed my very own web app from scratch using React. Please hire me.</h1>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
